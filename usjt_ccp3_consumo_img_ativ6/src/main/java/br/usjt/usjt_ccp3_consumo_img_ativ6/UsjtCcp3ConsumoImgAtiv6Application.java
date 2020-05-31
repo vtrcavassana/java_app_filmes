46908ed00ff5184de092fac183d913002fe905ff
vtrcavassana <victor.cavassana@outlook.com>
2020-05-31 03:21:58 -0300

    Atividade 06 iniciada

---
+++ usjt_ccp3_consumo_img_ativ6/src/main/java/br/usjt/usjt_ccp3_consumo_img_ativ6/UsjtCcp3ConsumoImgAtiv6Application.java
@@ -0,0 +1,13 @@
+package br.usjt.usjt_ccp3_consumo_img_ativ6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsjtCcp3ConsumoImgAtiv6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsjtCcp3ConsumoImgAtiv6Application.class, args);
+	}
+
+}
---
+++ usjt_ccp3_consumo_img_ativ6/src/test/java/br/usjt/usjt_ccp3_consumo_img_ativ6/UsjtCcp3ConsumoImgAtiv6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usjt.usjt_ccp3_consumo_img_ativ6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsjtCcp3ConsumoImgAtiv6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
